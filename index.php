ef73c640d5d2e1006de1a33855ef5c429b01d802
MH-Fahim11 <fmahmudulhasan@gmail.com>
2024-05-29 20:19:21 +0600

    php add

--- index.php
+++ index.php
@@ -46,177 +46,9 @@
     <div class="preloader">
         <div class="preloader__image" style="background-image: url(assets/images/loader.png);"></div>
     </div>
-        <header class="main-header sticky-header sticky-header--normal">
-            <div class="main-header__inner">
-                <div class="main-header__logo">
-                    <a href="index.html">
-                        <img src="assets/images/logos/logo-light.png" alt="Hiredots HTML" width="125">
-                    </a>
-                </div><!-- /.main-header__logo -->
-                <a href="#" class="search-toggler main-header__search">
-                    <i class="icon-magnifying-glass" aria-hidden="true"></i>
-                    <span class="sr-only">Search</span>
-                </a><!-- /.search-toggler -->
-                <div class="main-header__menu">
-                    <nav class="main-header__nav main-menu">
-                        <ul class="main-menu__list">
-                            <li class="dropdown">
-                                <a href="professionals.html">Professionels</a>
-                            </li>
-
-                            <li>
-                                <a href="entreprises.html">Entreprises</a>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#">Universities</a>
-                                <ul>
-                                    <li><a href="team-details.html">Team Details</a></li>
-                                    <li><a href="jobs.html">Jobs</a></li>
-                                    <li><a href="history.html">History</a></li>
-                                    <li><a href="faq.html">FAQs</a></li>
-                                </ul>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#">Services</a>
-                                <ul>
-                                    <li><a href="services.html">Services</a></li>
-                                    <li><a href="service-d-consulting.html">HR Consulting</a></li>
-                                    <li><a href="service-d-resources.html">Tech Resources</a></li>
-                                    <li><a href="service-d-hydro-solution.html">Staffing Solution</a></li>
-                                    <li><a href="service-d-program.html">Corporate Program</a></li>
-                                    <li> <a href="service-d-training.html">Leadership Training</a></li>
-                                    <li><a href="service-d-management.html">Staff Management</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="dropdown">
-                                <a href="project.html">Projects</a>
-                                <ul>
-                                    <li><a href="project.html">Projects</a></li>
-                                    <li><a href="project-details.html">Projects Details</a></li>
-                                </ul>
-                            </li>
-                            <!-- <li class="dropdown">
-                                <a href="#">News</a>
-                                <ul>
-                                    <li><a href="blog.html">News</a></li>
-                                    <li><a href="blog-details.html">News Details</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="contact.html">Contact</a>
-                            </li> -->
-                        </ul>
-                    </nav><!-- /.main-header__nav -->
-                    <!-- <div class="main-header__call">
-                        <i class="icon-telephone"></i>
-                        <a href="tel:+9288006930">+ 92 (8800)-6930</a>
-                    </div> -->
-                    <!-- <div style="background-color: aqua; margin: 5px; margin-right: 30px;">
-
-                    
-                        <div class=" d-flex align-items-center" style="background-color: white; margin: 5px; padding: 5px;">
-                            <div class="">
-                                <a>Connexion</a>
-                                <select class="form-select" aria-label="Default select example">
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
-                            </div>
-
-                            <div>
-                                <select class="form-select" aria-label="Default select example">
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div> -->
-                    <div class="connexion">
-                        <div class="connexion-i1">
     
-                            <p style="color: rgb(25, 164, 222);">Connexion</p>
-                            <div style="margin-top: -20px;">
-                                <select style="border: none;">
-                                    <option>Inscription</option>
-                                    <option>Entreprises</option>
-                                    <option>Professionnels</option>
-                                    <option>Universités</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="connexion-i2 lang-menu">
-                            
-                                <div class="selected-lang">
-                                </div>
-                                <ul>
-                                    <li>
-                                        <a href="#" class="fr"></a>
-                                    </li>
-                                    <li>
-                                        <a href="" class="en"></a>
-                                    </li>
-                                    <li>
-                                        <a href="" class="es"></a>
-                                    </li>
-                                    <li>
-                                        <a href="" class="ht"></a>
-                                    </li>
-                                </ul>
-                                
-                            
-                        </div>
-                    </div>
-                    
-                </div>
-                <!-- <div class="main-header__link">
-                    <a class="main-header__btn" href="contact.html">Book <br> Appointment</a>
-                    
-                </div> -->
-                <div class="connexion-m">
-                    <div class="connexion-i1-m">
-
-                        <p style="color: rgb(25, 164, 222);">Connexion</p>
-                        <div style="margin-top: -20px;">
-                            <select style="border: none;">
-                                <option>Inscription</option>
-                                <option>Entreprises</option>
-                                <option>Professionnels</option>
-                                <option>Universités</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="connexion-i2 lang-menu">
-                            
-                        <div class="selected-lang">
-                        </div>
-                        <ul>
-                            <li>
-                                <a href="#" class="fr"></a>
-                            </li>
-                            <li>
-                                <a href="" class="en"></a>
-                            </li>
-                            <li>
-                                <a href="" class="es"></a>
-                            </li>
-                            <li>
-                                <a href="" class="ht"></a>
-                            </li>
-                        </ul>
-                        
-                    
-                </div>
-                </div>
-
-                <div class="mobile-nav__btn mobile-nav__toggler">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div><!-- /.mobile-nav__toggler -->
-            </div>
+        <header id="main-header" class="main-header sticky-header sticky-header--normal">
+            
         </header>
         <!-- main-slider-start -->
         <section class="main-slider-one">
@@ -323,7 +155,7 @@
                     <div class="col-md-6 col-lg-4">
                         <div class="service-one-card service-card-two" data-wow-duration="1500ms" data-wow-delay="${index}ms">
                             <div class="service-one-card__image">
-                                <img src="../images/34FinalPhotos/IMG-20240320-WA0018.jpg" alt="${job.title}">
+                                <img src="./images/34FinalPhotos/IMG-20240320-WA0018.jpg" alt="${job.title}">
                             </div>
                             <div class="service-one-card__content">
                                 <div style="display: flex; justify-content: space-between" class="service-one-card__icon">
@@ -940,6 +772,7 @@ Time: 10:00 AM - 4:00 PM
     <script src="assets/vendors/jquery-circleType/jquery.circleType.js"></script>
     <script src="assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
     <script src="./scripts/home.js"></script>
+    <script src="./scripts/nav.js"></script>
     <!-- template js -->
     <script src="assets/js/hiredots.js"></script>
 </body>
