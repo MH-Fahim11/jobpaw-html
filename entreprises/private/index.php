eba11354d8c3b676c16b448c666cceede60a8c32
MH-Fahim11 <fmahmudulhasan@gmail.com>
2024-05-31 01:34:32 +0600

    new page adde

--- entreprises/private/index.php
+++ entreprises/private/index.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jobpaw | professionals</title>
     <!-- favicons Icons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="../../assets/images/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../../assets/images/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicons/favicon-16x16.png">
-    <link rel="manifest" href="../assets/images/favicons/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../assets/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../assets/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="../../assets/images/favicons/site.webmanifest">
     <meta name="description" content="Hiredots is a modern HTML Template HR services, Recruiting Agency, Recruiting Business, Hire Employees, Recruiting Consulting, Job Posting, Interviewing Services websites and businesses.">
 
     <!-- fonts -->
@@ -17,25 +17,25 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <!-- reey font -->
-    <link rel="stylesheet" href="../assets/vendors/reey-font/stylesheet.css">
+    <link rel="stylesheet" href="../../assets/vendors/reey-font/stylesheet.css">
 
 
-    <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/vendors/bootstrap-select/bootstrap-select.min.css">
-    <link rel="stylesheet" href="../assets/vendors/animate/animate.min.css">
-    <link rel="stylesheet" href="../assets/vendors/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="../assets/vendors/jquery-ui/jquery-ui.css">
-    <link rel="stylesheet" href="../assets/vendors/jarallax/jarallax.css">
-    <link rel="stylesheet" href="../assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css">
-    <link rel="stylesheet" href="../assets/vendors/nouislider/nouislider.min.css">
-    <link rel="stylesheet" href="../assets/vendors/nouislider/nouislider.pips.css">
-    <link rel="stylesheet" href="../assets/vendors/slick/slick.css">
-    <link rel="stylesheet" href="../assets/vendors/hiredots-icons/style.css">
-    <link rel="stylesheet" href="../assets/vendors/owl-carousel/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="../assets/vendors/owl-carousel/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/bootstrap-select/bootstrap-select.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/animate/animate.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/jquery-ui/jquery-ui.css">
+    <link rel="stylesheet" href="../../assets/vendors/jarallax/jarallax.css">
+    <link rel="stylesheet" href="../../assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css">
+    <link rel="stylesheet" href="../../assets/vendors/nouislider/nouislider.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/nouislider/nouislider.pips.css">
+    <link rel="stylesheet" href="../../assets/vendors/slick/slick.css">
+    <link rel="stylesheet" href="../../assets/vendors/hiredots-icons/style.css">
+    <link rel="stylesheet" href="../../assets/vendors/owl-carousel/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../assets/vendors/owl-carousel/css/owl.theme.default.min.css">
     
     <!-- template styles -->
-    <link rel="stylesheet" href="../assets/css/hiredots.css">
+    <link rel="stylesheet" href="../../assets/css/hiredots.css">
 </head>
 
 <body class="custom-cursor">
@@ -44,12 +44,12 @@
     <div class="custom-cursor__cursor-two"></div>
 
     <div class="preloader">
-        <div class="preloader__image" style="background-image: url(../assets/images/loader.png);"></div>
+        <div class="preloader__image" style="background-image: url(../../assets/images/loader.png);"></div>
     </div>
         <header  class="main-header sticky-header sticky-header--normal">
         <?php
-            $logo = "../assets/images/logos/logo-light.png";
-            include_once('../component/navBar.php') ; 
+            $logo = "../../../assets/images/logos/logo-light.png";
+            include_once('../../component/navBar.php') ; 
         ?>    
         </header>
         <!-- main-slider-start -->
@@ -74,19 +74,31 @@
         </div>
         <!-- main-slider-end -->
         <!-- Feature Start -->
+        <div class="container mt-5">
+            <h2>Welcome, Mehetaj Khandaker</h2>
+            <p>A partir de cette interface, vous pouvez mettre à jour votre profil. Vous découvrirez un univers d’opportunités: publier offre, appel d’offres, séminaire; trouver contrats et marchés; publicité via bannière web, publicité via blast-email; recruter via Head Hunter, recruter via accès au CV, recruter via publication, etc.</p> <br>
+            <p>La plateforme vous guide dans vos décisions: recrutement, achat, formation, publicité, coaching, etc. Si vous voulez changer votre e-mail (login), écrivez-nous sur: webmaster@jobpaw.com.</p>
+        </div>
        <div class="my-5">
         <section class="feature-one">
             <div class="container">
-                <div class="row gutter-y-30" id="entreprises-container">
+                <div class="row gutter-y-30" id="entreprises-private-container">
                     <!-- JavaScript will populate this container -->
                 </div>
             </div>
         </section>
        </div>
-
+        <div class="container">
+            <p>Nous vous invitons à faire partie des contributeurs de la plateforme JobPaw en:</p>
+            <ul>
+                <li>Partageant, avec le bouton share, les pages de JobPaw avec vos contacts e-mail, Facebook, Twitter, Instagram, LinkedIn, etc.</li>
+                <li>Commentant, avec Disqus, les pages de JobPaw.com.</li>
+                <li>Suivant nos pages Facebook, Twitter, Instagram, LinkedIn</li>
+            </ul>
+        </div>
        
        <?php
-            include_once('../component/footer.php') ; 
+            include_once('../../component/footer.php') ; 
         ?> 
 
     </div>
@@ -100,7 +112,7 @@
         <div class="mobile-nav__content">
             <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
             <div class="logo-box">
-                <a href="index.html" aria-label="logo image"><img src="../assets/images/logos/logo-for-dark.png" width="155" alt=""></a>
+                <a href="index.html" aria-label="logo image"><img src="../../assets/images/logos/logo-for-dark.png" width="155" alt=""></a>
             </div>
             <!-- /.logo-box -->
             <div class="mobile-nav__container"></div>
@@ -158,32 +170,30 @@
     </a> -->
 
 
-    <script src="../assets/vendors/jquery/jquery-3.7.0.min.js"></script>
-    <script src="../assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
-    <script src="../assets/vendors/jarallax/jarallax.min.js"></script>
-    <script src="../assets/vendors/jquery-ui/jquery-ui.js"></script>
-    <script src="../assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
-    <script src="../assets/vendors/jquery-appear/jquery.appear.min.js"></script>
-    <script src="../assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
-    <script src="../assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
-    <script src="../assets/vendors/jquery-validate/jquery.validate.min.js"></script>
-    <script src="../assets/vendors/nouislider/nouislider.min.js"></script>
-    <script src="../assets/vendors/slick/slick.min.js"></script>
-    <script src="../assets/vendors/wnumb/wNumb.min.js"></script>
-    <script src="../assets/vendors/owl-carousel/js/owl.carousel.min.js"></script>
-    <script src="../assets/vendors/wow/wow.js"></script>
-    <script src="../assets/vendors/imagesloaded/imagesloaded.min.js"></script>
-    <script src="../assets/vendors/isotope/isotope.js"></script>
-    <script src="../assets/vendors/countdown/countdown.min.js"></script>
-    <script src="../assets/vendors/jquery-circleType/jquery.circleType.js"></script>
-    <script src="../assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
-    
-    <script src="../scripts/nav.js"></script>
+    <script src="../../assets/vendors/jquery/jquery-3.7.0.min.js"></script>
+    <script src="../../assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+    <script src="../../assets/vendors/jarallax/jarallax.min.js"></script>
+    <script src="../../assets/vendors/jquery-ui/jquery-ui.js"></script>
+    <script src="../../assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
+    <script src="../../assets/vendors/jquery-appear/jquery.appear.min.js"></script>
+    <script src="../../assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
+    <script src="../../assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="../../assets/vendors/jquery-validate/jquery.validate.min.js"></script>
+    <script src="../../assets/vendors/nouislider/nouislider.min.js"></script>
+    <script src="../../assets/vendors/slick/slick.min.js"></script>
+    <script src="../../assets/vendors/wnumb/wNumb.min.js"></script>
+    <script src="../../assets/vendors/owl-carousel/js/owl.carousel.min.js"></script>
+    <script src="../../assets/vendors/wow/wow.js"></script>
+    <script src="../../assets/vendors/imagesloaded/imagesloaded.min.js"></script>
+    <script src="../../assets/vendors/isotope/isotope.js"></script>
+    <script src="../../assets/vendors/countdown/countdown.min.js"></script>
+    <script src="../../assets/vendors/jquery-circleType/jquery.circleType.js"></script>
+    <script src="../../assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
 
-    <script src="../scripts/entreprises.js"></script>
+    <script src="../../scripts/entreprisesPrivate.js"></script>
     <!-- template js -->
-    <script src="../assets/js/hiredots.js"></script>
+    <script src="../../assets/js/hiredots.js"></script>
 </body>
 
 </html>
