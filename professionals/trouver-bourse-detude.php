162bea0e1153634b4b22a02dfe26fbf75b5deb44
MH-Fahim11 <fmahmudulhasan@gmail.com>
2024-06-01 23:22:21 +0600

    many fix

--- professionals/trouver-bourse-detude.php
+++ professionals/trouver-bourse-detude.php
@@ -892,172 +892,20 @@
             </div>
         </section>
     </div>
-    <footer class="main-footer background-black">
-        <div class="main-footer__bg" style="background-image: url(../assets/images/backgrounds/footer-bg.jpg);"></div>
-        <!-- /.main-footer__bg -->
-        <div class="main-footer__subscribe">
-            <div class="container">
-                <div class="main-footer__subscribe__wrapper">
-                    <div class="main-footer__subscribe__left">
-                        <span class="main-footer__subscribe__icon"><i class="icon-message"></i></span>
-                        <h3 class="main-footer__subscribe__title">Subscribe Now to Get <br>
-                            Latest Updates</h3>
-                    </div>
-                    <div class="main-footer__subscribe__right">
-                        <form action="#" data-url="MAILCHIMP_FORM_URL" class="main-footer__newsletter mc-form">
-                            <input type="text" name="EMAIL" placeholder="Email address">
-                            <button type="submit" class="fas fa-paper-plane">
-                                <span class="sr-only">submit</span><!-- /.sr-only -->
-                            </button>
-                        </form><!-- /.footer-widget__newsletter mc-form -->
-                        <div class="mc-form__response"></div><!-- /.mc-form__response -->
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="main-footer__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-xl-5">
-                        <div class="footer-widget footer-widget--about">
-                            <a href="index.html" class="footer-widget__logo">
-                                <img src="../assets/images/logo-light.png" width="125" alt="Hiredots HTML Template">
-                            </a>
-                            <p class="footer-widget__text">Lorem ipsum dolor sit amet, consect etur adi pisicing elit
-                                <br>
-                                sed do eiusmod tempor incididunt ut labore.
-                            </p>
-                            <div class="footer-widget__author">
-                                <div class="footer-widget__author__img">
-                                    <img src="../assets/images/resources/footer-about-avata.png" alt="">
-                                </div>
-                                <div>
-                                    <div class="footer-widget__author__info">
-                                        <span class="footer-widget__author__tagline">Need Help?</span>
-                                        <h3 class="footer-widget__author__title"><a href="contact.html">Book Appointment
-                                            </a></h3>
-                                    </div>
-                                </div>
-                            </div>
-                        </div><!-- /.footer-widget -->
-                    </div><!-- /.col-md-6 -->
-                    <div class="col-md-6 col-xl-2">
-                        <div class="footer-widget footer-widget--links">
-                            <h2 class="footer-widget__title">Explore</h2><!-- /.footer-widget__title -->
-                            <ul class="list-unstyled footer-widget__links">
-                                <li><a href="about.html">About Company</a></li>
-                                <li><a href="team.html">Meet the Team</a></li>
-                                <li><a href="blog.html">News & Media</a></li>
-                                <li><a href="project.html">Our Projects</a></li>
-                                <li><a href="contact.html">Contact</a></li>
-                            </ul><!-- /.list-unstyled footer-widget__links -->
-                        </div><!-- /.footer-widget -->
-                    </div><!-- /.col-md-6 -->
-                    <div class="col-md-6 col-xl-2">
-                        <div class="footer-widget footer-widget--contact">
-                            <h2 class="footer-widget__title">Links</h2><!-- /.footer-widget__title -->
-                            <ul class="list-unstyled footer-widget__links">
-                                <li><a href="about.html">Apply Now</a></li>
-                                <li><a href="contact.html">Contract to Hire</a></li>
-                                <li><a href="team.html">Direct Hire</a></li>
-                                <li><a href="history.html">Executive Search</a></li>
-                                <li><a href="about.html">Our Solutions</a></li>
-                            </ul><!-- /.list-unstyled footer-widget__links -->
-                        </div><!-- /.footer-widget -->
-                    </div><!-- /.col-md-6 -->
-                    <div class="col-md-6 col-xl-3">
-                        <div class="footer-widget footer-widget--time">
-                            <h2 class="footer-widget__title">Contact</h2><!-- /.footer-widget__title -->
-                            <p class="footer-widget__text">66 Road Broklyn Street, 600 <br> New York, USA</p>
-                            <!-- /.footer-widget__text -->
-                            <ul class="list-unstyled footer-widget__info">
-                                <li><i class="fas fa-envelope"></i> <a
-                                        href="mailto:needhelp@company.com">needhelp@company.com</a></li>
-                                <li><i class="fas fa-phone-square"></i> <a href="tel:+92(8800)-6930">+ 92 ( 8800 ) -
-                                        6930</a></li>
-                            </ul><!-- /.list-unstyled -->
-                        </div><!-- /.footer-widget -->
-                    </div><!-- /.col-md-6 -->
-                </div><!-- /.row -->
-            </div><!-- /.container -->
-        </div><!-- /.main-footer__top -->
-        <div class="main-footer__bottom">
-            <div class="container">
-                <div class="main-footer__bottom__inner">
-                    <div class="footer-widget__social">
-                        <a href="https://twitter.com">
-                            <i class="fab fa-twitter" aria-hidden="true"></i>
-                            <span class="sr-only">Twitter</span>
-                        </a>
-                        <a href="https://facebook.com">
-                            <i class="fab fa-facebook" aria-hidden="true"></i>
-                            <span class="sr-only">Facebook</span>
-                        </a>
-                        <a href="https://pinterest.com">
-                            <i class="fab fa-pinterest-p" aria-hidden="true"></i>
-                            <span class="sr-only">Pinterest</span>
-                        </a>
-                        <a href="https://instagram.com">
-                            <i class="fab fa-instagram" aria-hidden="true"></i>
-                            <span class="sr-only">Instagram</span>
-                        </a>
-                    </div><!-- /.footer-widget__social -->
-                    <p class="main-footer__copyright">
-                        &copy; Copyright <span class="dynamic-year"></span> by Hiredots HTML Template.
-                    </p>
-                </div><!-- /.main-footer__inner -->
-            </div><!-- /.container -->
-        </div><!-- /.main-footer__bottom -->
-    </footer><!-- /.main-footer -->
+    <?php
+            $logo ="../assets/images/logos/logo-dark.png";
+            include_once('./../component/footer.php') ; 
+        ?><!-- /.main-footer -->
 
     </div>
     <!-- /.page-wrapper -->
 
 
 
-    <div class="mobile-nav__wrapper">
-        <div class="mobile-nav__overlay mobile-nav__toggler"></div>
-        <!-- /.mobile-nav__overlay -->
-        <div class="mobile-nav__content">
-            <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
-            <div class="logo-box">
-                <a href="index.html" aria-label="logo image"><img src="../assets/images/logos/logo-for-dark.png"
-                        width="155" alt=""></a>
-            </div>
-            <!-- /.logo-box -->
-            <div class="mobile-nav__container"></div>
-            <!-- /.mobile-nav__container -->
-            <ul class="mobile-nav__contact list-unstyled">
-                <li>
-                    <i class="fa fa-envelope"></i>
-                    <a href="mailto:needhelp@hiredots.com">needhelp@hiredots.com</a>
-                </li>
-                <li>
-                    <i class="fa fa-phone-alt"></i>
-                    <a href="tel:666-888-0000">666 888 0000</a>
-                </li>
-            </ul><!-- /.mobile-nav__contact -->
-            <div class="mobile-nav__social">
-                <a href="https://twitter.com">
-                    <i class="fab fa-twitter" aria-hidden="true"></i>
-                    <span class="sr-only">Twitter</span>
-                </a>
-                <a href="https://facebook.com">
-                    <i class="fab fa-facebook" aria-hidden="true"></i>
-                    <span class="sr-only">Facebook</span>
-                </a>
-                <a href="https://pinterest.com">
-                    <i class="fab fa-pinterest-p" aria-hidden="true"></i>
-                    <span class="sr-only">Pinterest</span>
-                </a>
-                <a href="https://instagram.com">
-                    <i class="fab fa-instagram" aria-hidden="true"></i>
-                    <span class="sr-only">Instagram</span>
-                </a>
-            </div><!-- /.mobile-nav__social -->
-        </div>
-        <!-- /.mobile-nav__content -->
-    </div>
+    <?php
+            $logo ="../assets/images/logos/logo-dark.png";
+            include_once('./../component/mobileNav.php') ; 
+        ?>
     <!-- /.mobile-nav__wrapper -->
     <div class="search-popup">
         <div class="search-popup__overlay search-toggler"></div>
